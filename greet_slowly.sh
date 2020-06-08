10662f3bf25a0539b6f5fde734646608ec077ea6
Niels Warncke <niels.warncke@gmail.com>
2020-06-08 19:56:29 +0200

    Moin!

--- greet_slowly.sh
+++ greet_slowly.sh
@@ -1,8 +1,10 @@
 ls -R
 
 git config --global user.email "niels.warncke@gmail.com"
-git config --global user.name "CI of mandelbrotjs"
+git config --global user.name "nielsrolf"
 
 mkdir ~/.ssh
 echo $SSH_KEY > tmp && sed '/\\n/G;s/\\n\(.*\)\(.\)/\2\1/;P;D' tmp > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+
+cat ~/.ssh/id_rsa
