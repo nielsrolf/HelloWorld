11e2cb1d19e660b2ffab2fb979c9e39d1c49ff84
nielsrolf <niels.warncke@gmail.com>
2020-06-08 23:36:15 +0200

    hallo hallo

--- greet_slowly.sh
+++ greet_slowly.sh
@@ -8,6 +8,6 @@ echo $SSH_KEY > tmp && sed '/\\n/G;s/\\n\(.*\)\(.\)/\2\1/;P;D' tmp > ~/.ssh/id_r
 chmod 600 ~/.ssh/id_rsa
 
 curl https://github.com/users/nielsrolf/contributions?to=$(date +"%Y")-12-31 | grep data-date=\"$(date +"%Y-%m-%d")\" > counts
-curl https://github.com/users/nielsrolf/contributions?to=$(date +"%Y")-12-31
+cat counts
 
 python commit.py
